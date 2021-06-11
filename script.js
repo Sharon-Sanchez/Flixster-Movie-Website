045ff2e885a98f314fbc2fa0ad86296caa49b203
Sharon Sanchez <sharonsanchez581@gmail.com>
2021-06-11 10:34:12 -0700

    Title and Votes are now displayed

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 // HTML element constants 
 const moviesDisplay = document.querySelector("#movie-area");
 
-//Constants for API URL
-APIkey = "2683ad1e390b432952c03f1db25a3d7c";
 
-//Function that acquires data from API 
-async function getMovies(){
-    const apiURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + APIkey;
-    const movies = await fetch(apiURL);
-    const moviesData = await movies.json();
-    const results = moviesData.results;
-    
-    displayMovies(results);
+//Constants for API URL creation
+let APIkey = '?api_key=2683ad1e390b432952c03f1db25a3d7c';
+let BASE_URL = "https://api.themoviedb.org";
+let nowPlayingPath = "/3/movie/now_playing";
+
+function URLbuild(path){
+   const url = BASE_URL+path+ APIkey;
+   return url; 
 }
 
+async function getMovies(){
 
+    const url = URLbuild(nowPlayingPath);
+    const movies = await fetch(url);
+    const moviesData = await movies.json();
+    const results = moviesData.results;
+    console.log(results)
+    displayMovies(results); 
+    
+    
+}
+
+//Displays the movie posters
 function displayMovies(movies) {
     movies.forEach(element => {
-        moviesDisplay.innerHTML+=`<img src="https://image.tmdb.org/t/p/w500${element.poster_path} "alt=${movies.title}/>;`;
+        moviesDisplay.innerHTML +=`
+
+        <div id="poster"> 
+            <img src="https://image.tmdb.org/t/p/w500${element.poster_path} "alt=${movies.title}/>;
+        </div>
+        <div id="movieInfo">
+            <h4 id>${element.title}<h4> 
+            <h4 id>${element.vote_average}<h4>
+            
+        </div>
+    
+        `;
+
+        
     });
     
     
@@ -26,4 +49,5 @@ function displayMovies(movies) {
 
 
 //Activates the page 
-getMovies();
+getMovies(); 
+
