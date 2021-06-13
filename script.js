cb9b8b1d4bcf4ba6c51bb9daac2f78dfce879c62
Sharon Sanchez <sharonsanchez581@gmail.com>
2021-06-13 05:38:48 -0700

    Movie search,showmore,and near usability functions

--- script.js
+++ script.js
@@ -1,94 +1,96 @@
-// HTML element constants 
+// HTML elements to fill the page 
+
 const moviesDisplay = document.querySelector("#movie-area");
-const searchMovie = document.querySelector("search-area");
-const movieForm = document.querySelector("form");
+const movieForm = document.querySelector("#search-form");
+const showMoreResultsBtn = document.querySelector("#show-more-results-btn")
+const movieSearchInput = document.querySelector("#search-input");
+const showMoreBtn = document.querySelector("#show-more-btn")
+const secondHeader = document.getElementById('now-playing');
+const resultsHeader = document.getElementById('search-results');
+
 
 //Constants for API URL creation
-let APIkey = '?api_key=2683ad1e390b432952c03f1db25a3d7c';
-let BASE_URL = "https://api.themoviedb.org";
+//Reminder of url formet: Base url + path + apikey
+let APIkey = '2683ad1e390b432952c03f1db25a3d7c';
 let nowPlayingPath = "/3/movie/now_playing";
-let searchmovie = "/3/search/movie";
+let searchMoviePath = "/3/search/movie";
+let currentPage = 1;
+var searchTerm = '';
 
-
-
-function URLbuild(path){
-   const url = BASE_URL+path+ APIkey;
-   return url; 
-}
-
-async function getMovies(){
-
-    const url = URLbuild(nowPlayingPath);
-    const movies = await fetch(url);
-    const moviesData = await movies.json();
-    const results = moviesData.results;
-    console.log(results)
-    displayMovies(results); 
-    
+//This function builds the API URL and retrieves the JSON Data from it
+async function getMovieApiData(path){
+    if (path == searchMoviePath) {
+        const movieURL = await fetch(`https://api.themoviedb.org${path}?api_key=${APIkey}&page=${currentPage}&query=${searchTerm}`);
+        const movieData = await movieURL.json();
+        return movieData.results;
+    } 
+    else{
+        const movieURL = await fetch(`https://api.themoviedb.org${path}?api_key=${APIkey}&page=${currentPage}`); 
+        const movieData = await movieURL.json();
+        return movieData.results;
+    }
     
 }
 
-movieForm.addEventListener("submit",movieSearch);
+//After it receives the data from the URL, It will display the movie info: poster, name, and vote average.
+async function displayMovies(moviePath){
+    console.log("Getting Data..");
+    let movieArray = await getMovieApiData(moviePath);
+    //console.log(movieArray);
 
-async function movieSearch(event) {
-    event.preventDefault();
-    const movieInput = event.target.movieSearch;
-    const moviesInfo = movieInput.value;
-    const apiurl= BASE_URL + searchmovie + APIkey + "&query=" + moviesInfo;
-
-    const response = await fetch(apiurl);
-    const responseData = await response.json();
-    const dataResults = responseData.results;
-    console.log(dataResults); 
-    search(dataResults);
-}
-
-function search(movies) {
-    movies.forEach(element => {
-        searchMovie.innerHTML +=`
-
-        <div class="display"> 
-            <div class="poster"> 
-                <img src="https://image.tmdb.org/t/p/w500${element.poster_path} "alt=${movies.title}/>
-            </div>
-            <div class="movieInfo">
-                <h4 style="width: 88%; text-align: center; clear: left;">${element.title}<h4> 
-                <h4 style="width: 88%; text-align: center; clear: left;">${element.vote_average}<h4>
-            </div>
-        </div>
-    
-        `;
-
-        
-    });
-    
-    
-}
-//Displays the movie posters, title, and votes
-function displayMovies(movies) {
-    movies.forEach(element => {
+    for (let index = 0; index < movieArray.length; index++) {
+        console.log(movieArray[index]);
         moviesDisplay.innerHTML +=`
 
         <div class="display"> 
             <div class="poster"> 
-                <img src="https://image.tmdb.org/t/p/w500${element.poster_path} "alt=${movies.title}/>
+                <img src="https://image.tmdb.org/t/p/w500${movieArray[index].poster_path} "alt=${movieArray[index].title}/>
             </div>
             <div class="movieInfo">
-                <h4 style="width: 88%; text-align: center; clear: left;">${element.title}<h4> 
-                <h4 style="width: 88%; text-align: center; clear: left;">${element.vote_average}<h4>
+                <h3 style="width: 100%; text-align: center; clear: left;">${movieArray[index].title}<h3> 
+                <h3 style="width: 95%; text-align: center; clear: left;">⭐️${movieArray[index].vote_average}<h3>
             </div>
         </div>
-    
-        `;
-
         
-    });
-    
-    
+        `;        
+    }
+    currentPage ++;
+
 }
 
 
+//Active Functions that are being displayed on the webpage
 
-//Activates the page 
-getMovies(); 
+//Page the user first sees, the now playing in theaters movies
+displayMovies(nowPlayingPath);
+
+async function movieSearch(event){
+    event.preventDefault();
+    moviesDisplay.innerHTML = '';
+    secondHeader.classList.add("hidden")
+    resultsHeader.classList.remove("hidden");
+    showMoreBtn.classList.add('hiddenBtn');
+    showMoreResultsBtn.classList.remove('hiddenBtn');
+    searchTerm = movieSearchInput.value;
+    await displayMovies(searchMoviePath);
+    movieSearchInput.value = '';
+    currentPage++;
+
+}
+movieForm.addEventListener('submit',movieSearch);
+
+//The user clicks on Show more to see movie options available
+async function showMore(event) {
+    await displayMovies(nowPlayingPath);
+    currentPage ++;
+}
+showMoreBtn.addEventListener('click',showMore);   
+
+//User clicks the show more results when they are looking for a movie via submit form
+async function showMoreResults(event){
+    await displayMovies(searchMoviePath);
+    currentPage++;
+}
+showMoreResultsBtn.addEventListener('click',showMoreResults);
+
 
